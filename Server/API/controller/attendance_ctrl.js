488e84964d7e5d80a015201de500d74abe2d6181
Wushoooo <baribartan@gmail.com>
2025-02-06 14:20:50 +0800

    test Create
    Create

---
+++ Server/API/controller/attendance_ctrl.js
@@ -0,0 +1,49 @@
+const Attendance = require('../models')
+const User = require('../models')
+
+//Imports
+const util = require('../../utils')
+const Op = require('sequelize')
+
+const addAttendance = async (req, res, next){
+
+    try {
+
+        // status present or absent
+        // timestamp time created at
+        //  
+    } catch (error) {
+        
+    }
+
+}
+
+const updateAttendance = async (req, res, next){
+
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
+
+const deleteAttendance = async (req, res, next){
+    
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
+
+const getAttendance = async (req, res, next){
+    
+    try {
+        
+    } catch (error) {
+        
+    }
+
+}
--- Server/server.js
+++ Server/server.js
@@ -7,13 +7,13 @@ const port = 8080;
 const server = http.createServer(app);
 
 // Synchronize the database before starting the server
-db.sequelize.sync({ alter: true }) // Change `force` to `true` only for development or testing
+db.sequelize.sync({ alter: false }) // Change `force` to `true` only for development or testing
   .then(() => {
     console.log('Database connected and synchronized successfully.');
     server.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
   })
-  .catch((err) => {
+  .catch((err) => {2
     console.error('Failed to connect and sync database:', err);
   });
